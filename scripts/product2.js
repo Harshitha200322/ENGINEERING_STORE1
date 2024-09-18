ad69b1a0834df190636312d431fa054cc84f3932
Harshitha200322 <harshitha5675@gmail.com>
2024-09-18 16:25:16 +0530

    first commit

---
+++ scripts/product2.js
@@ -0,0 +1,121 @@
+const mobiles = [
+    {
+        id : 1,
+        name : 'COMPUTER NETWORKS',
+        price : 200,
+        inStock : 10,
+        imgSrc : "images/casio.jpg"
+    },
+
+    {
+        id : 2,
+        name : 'Design &analsyis of algorithms',
+        price : 150,
+        inStock : 10,
+        imgSrc : "images/draf.jpeg"
+    },
+
+    {
+        id : 3,
+        name : 'DATABASE SYSTEM',
+        price : 150,
+        inStock : 8,
+        imgSrc : "images/draf2.jpeg"
+    },
+
+    {
+        id : 4,
+        name : 'Digital Logic design',
+        price : 150,
+        inStock : 8,
+        imgSrc : "images/holder.jpg"
+    },
+
+    {
+        id : 5,
+        name : 'Operating System',
+        price : 250,
+        inStock : 5,
+        imgSrc : "images/image3.jpeg"
+    },
+
+    {
+        id : 6,
+        name : 'Discrete Mathematics',
+        price : 180,
+        inStock : 10,
+        imgSrc : "images/image55.jpeg"
+    },
+
+    {
+        id : 7,
+        name : 'Data Structures',
+        price : 100,
+        inStock : 12,
+        imgSrc : "images/image66.jpeg"
+    },
+
+    {
+        id : 8,
+        name : 'OBJECT ORIENTED JAVA',
+        price : 200,
+        inStock : 4,
+        imgSrc : "images/image555.jpeg"
+    },
+
+    {
+        id : 9,
+        name : 'AUTOMETA. THEORY',
+        price : 830,
+        inStock : 6,
+        imgSrc : "images/casio.jpg"
+    }];
+const gameConsoles = [
+    {
+        id : 10,
+        name : 'PS4',
+        price : 259,
+        inStock : 10,
+        imgSrc : "images/draf.jpeg"
+    },
+
+    {
+        id : 11,
+        name : 'PS5',
+        price : 399,
+        inStock : 15,
+        imgSrc : "images/draf2.jpeg"
+    },
+
+    {
+        id : 12,
+        name : 'Xbox-seriesX',
+        price : 450,
+        inStock : 9,
+        imgSrc : "images/holder.jpg"
+    },
+
+    {
+        id : 13,
+        name : 'Nintendo Switch',
+        price : 250,
+        inStock : 7,
+        imgSrc : "images/image3.jpeg"
+    },
+
+    {
+        id : 14,
+        name : 'PS4 Controller',
+        price : 50,
+        inStock : 4,
+        imgSrc : "images/image55.jpeg"
+    },
+
+    {
+        id : 15,
+        name : 'PS5 Controller',
+        price : 70,
+        inStock : 10,
+        imgSrc : "images/image66.jpeg"
+    }
+];
---
+++ scripts/products.js
@@ -0,0 +1,121 @@
+const mobiles = [
+    {
+        id : 1,
+        name : 'COMPUTER NETWORKS',
+        price : 200,
+        inStock : 10,
+        imgSrc : "images/cn.jpg"
+    },
+
+    {
+        id : 2,
+        name : 'Design &analsyis of algorithms',
+        price : 150,
+        inStock : 10,
+        imgSrc : "images/daa.jpg"
+    },
+
+    {
+        id : 3,
+        name : 'DATABASE SYSTEM',
+        price : 150,
+        inStock : 8,
+        imgSrc : "images/dbms.jpeg"
+    },
+
+    {
+        id : 4,
+        name : 'Digital Logic design',
+        price : 150,
+        inStock : 8,
+        imgSrc : "images/dld.jpg"
+    },
+
+    {
+        id : 5,
+        name : 'Operating System',
+        price : 250,
+        inStock : 5,
+        imgSrc : "images/os.jpg"
+    },
+
+    {
+        id : 6,
+        name : 'Discrete Mathematics',
+        price : 180,
+        inStock : 10,
+        imgSrc : "images/dismat.jpeg"
+    },
+
+    {
+        id : 7,
+        name : 'Data Structures',
+        price : 100,
+        inStock : 12,
+        imgSrc : "images/ds.jpeg"
+    },
+
+    {
+        id : 8,
+        name : 'OBJECT ORIENTED JAVA',
+        price : 200,
+        inStock : 4,
+        imgSrc : "images/java.jpg"
+    },
+
+    {
+        id : 9,
+        name : 'AUTOMETA. THEORY',
+        price : 830,
+        inStock : 6,
+        imgSrc : "images/java.jpg"
+    }];
+const gameConsoles = [
+    {
+        id : 10,
+        name : 'PS4',
+        price : 259,
+        inStock : 10,
+        imgSrc : "images/ds.jpeg"
+    },
+
+    {
+        id : 11,
+        name : 'PS5',
+        price : 399,
+        inStock : 15,
+        imgSrc : "images/dismat.jpeg"
+    },
+
+    {
+        id : 12,
+        name : 'Xbox-seriesX',
+        price : 450,
+        inStock : 9,
+        imgSrc : "images/os.jpg"
+    },
+
+    {
+        id : 13,
+        name : 'Nintendo Switch',
+        price : 250,
+        inStock : 7,
+        imgSrc : "images/cn.jpg"
+    },
+
+    {
+        id : 14,
+        name : 'PS4 Controller',
+        price : 50,
+        inStock : 4,
+        imgSrc : "images/daa.jpg"
+    },
+
+    {
+        id : 15,
+        name : 'PS5 Controller',
+        price : 70,
+        inStock : 10,
+        imgSrc : "images/dbms.jpeg"
+    }
+];
